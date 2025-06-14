4e85c164b5be2767fc90f84314cfeefb261d2350
robzjr <aassddffgghgg@gmail.com>
2025-06-14 23:17:50 +0300

    Add SpeedInsights component and update configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+function Main() {
+  return (
+    <>
+      <SpeedInsights />
+      <App />
+    </>
+  );
+}
+
+root.render(<Main />);
